84a4f646becad4723cc21b103250fbf08db5fb78
0xlaguna <lagunamichael2@outlook.com>
2024-07-18 03:25:08 +0000

    feat: add util for password hashing

--- quark/src/lib.rs
+++ quark/src/lib.rs
@@ -1,9 +1,13 @@
 #[macro_use]
 extern crate schemars;
+#[macro_use]
+extern crate lazy_static;
 
 pub use iso8601_timestamp::Timestamp;
 pub use sea_orm;
 
 pub mod util;
+
+pub mod auth;
 
 pub use util::result::{Error, Result};
